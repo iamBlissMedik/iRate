e3dbdf199904b604676b1809701f155f5378472b
Blizz <ellamsbliss@gmail.com>
2025-11-23 13:35:23 +0100

    sidebar with icons

--- src/constants/navLinks.ts
+++ src/constants/navLinks.ts
@@ -1,37 +1,54 @@
+import {
+  LayoutDashboard,
+  Users,
+  CreditCard,
+  BarChart3,
+  Settings,
+  LifeBuoy,
+  LogOut,
+} from "lucide-react";
+
 export const sidebarLinks = [
   {
     id: "dashboard",
     title: "Dashboard",
-    url: "/dashboard",
+    url: "#",
+    icon: LayoutDashboard,
   },
   {
     id: "users",
     title: "Users",
     url: "#",
+    icon: Users,
   },
   {
     id: "transactions",
     title: "Transactions",
     url: "#",
+    icon: CreditCard,
   },
   {
     id: "analytics",
     title: "Analytics",
     url: "#",
+    icon: BarChart3,
   },
   {
     id: "settings",
     title: "Settings",
     url: "#",
+    icon: Settings,
   },
   {
     id: "support",
     title: "Support",
     url: "#",
+    icon: LifeBuoy,
   },
   {
     id: "logout",
     title: "Logout",
     url: "#",
+    icon: LogOut,
   },
 ];
