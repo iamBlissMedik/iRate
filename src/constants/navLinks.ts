dd810e6b00a202b5c60bfb41851c9b8a23ff160f
Blizz <ellamsbliss@gmail.com>
2025-11-23 12:24:20 +0100

    update sidebar navitems

---
+++ src/constants/navLinks.ts
@@ -0,0 +1,37 @@
+export const sidebarLinks = [
+  {
+    id: "dashboard",
+    title: "Dashboard",
+    url: "/dashboard",
+  },
+  {
+    id: "users",
+    title: "Users",
+    url: "#",
+  },
+  {
+    id: "transactions",
+    title: "Transactions",
+    url: "#",
+  },
+  {
+    id: "analytics",
+    title: "Analytics",
+    url: "#",
+  },
+  {
+    id: "settings",
+    title: "Settings",
+    url: "#",
+  },
+  {
+    id: "support",
+    title: "Support",
+    url: "#",
+  },
+  {
+    id: "logout",
+    title: "Logout",
+    url: "#",
+  },
+];
