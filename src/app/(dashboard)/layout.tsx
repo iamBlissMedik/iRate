fad82bde6006f3e28bb60ff93f1b9b39787bce89
Blizz <ellamsbliss@gmail.com>
2025-11-23 08:59:52 +0100

    architecture

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,9 @@
+import DashboardLayoutClient from "@/components/DashboardLayout";
+
+export default async function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <DashboardLayoutClient>{children}</DashboardLayoutClient>;
+}
--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -2,11 +2,14 @@
 import { ReactNode } from "react";
 import { ThemeProvider } from "./ThemeProvider";
 import { Toaster } from "react-hot-toast";
+import { SessionProvider } from "next-auth/react";
 export function Providers({ children }: { children: ReactNode }) {
   return (
-    <ThemeProvider>
-      {children}
-      <Toaster position="top-right" />
-    </ThemeProvider>
+    <SessionProvider>
+      <ThemeProvider>
+        {children}
+        <Toaster position="top-right" />
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
