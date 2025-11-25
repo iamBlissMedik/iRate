3d0fba2bf22c294dfc9cf57a0be6bb2b3735ca97
Blizz <ellamsbliss@gmail.com>
2025-11-25 05:51:25 +0100

    layout

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -18,7 +18,7 @@ const DashboardLayoutClient = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex h-screen mx-auto max-w-full  ">
       <Sidebar />
-      <main className="flex-1 overflow-auto w-full px-[59px] pt-10 pb-16">
+      <main className="flex-1 overflow-auto w-full px-[59px] pt-10 pb-16 lg:ml-64 ">
         <div className=" mx-auto">{children}</div>
       </main>
     </div>
