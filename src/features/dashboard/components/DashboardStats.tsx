497e7f19b4c798111bc7bfb01c5b8c25f54c00cc
Blizz <ellamsbliss@gmail.com>
2026-02-12 16:21:07 +0100

    folder structure

--- src/features/dashboard/components/DashboardStats.tsx
+++ src/features/dashboard/components/DashboardStats.tsx
@@ -3,20 +3,21 @@
 import DashboardStatCard from "@/shared/components/ui/CustomDashboardStatCard";
 import { useDashboardOverview } from "@/features/dashboard/hooks/overview.hooks";
 import StatSkeleton from "@/shared/components/ui/CustomStatSkeleton";
-import { adaptOverviewToDashboardStats } from "../services/dashboardStats.adapter";
+import { adaptOverviewToDashboardStats } from "../adapters";
+
 
 export default function DashboardStats() {
   const { data, isLoading, isError } = useDashboardOverview();
 
   if (isLoading) {
-  return (
-    <section className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-5 gap-6">
-      {Array.from({ length: 5 }).map((_, i) => (
-        <StatSkeleton key={i} />
-      ))}
-    </section>
-  );
-}
+    return (
+      <section className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-5 gap-6">
+        {Array.from({ length: 5 }).map((_, i) => (
+          <StatSkeleton key={i} />
+        ))}
+      </section>
+    );
+  }
 
   if (isError || !data) {
     return (
